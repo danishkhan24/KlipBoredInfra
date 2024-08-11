c38bb0f39dcdece6cbcf43674179309d94750f06
Danish Khan <danishkhand24@gmail.com>
2024-08-11 23:12:53 +0100

    update providers

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"  # Use a consistent version across your modules
     }
   }
 
-  required_version = ">= 0.14"
+  required_version = ">= 1.0"
 }
 
 provider "aws" {
