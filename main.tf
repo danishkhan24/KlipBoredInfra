7e193e5d329a9fc116f1850dae38814be1fbc311
Danish Khan <danishkhand24@gmail.com>
2024-08-31 11:59:12 +0100

    Updated app name in fargate profile

--- main.tf
+++ main.tf
@@ -288,17 +288,17 @@ resource "aws_eks_fargate_profile" "system_fargate_profile" {
   }
 }
 
-# EKS Fargate Profile for Application
+# EKS Fargate Profile for Monitoring
 resource "aws_eks_fargate_profile" "monitoring_fargate_profile" {
   cluster_name           = aws_eks_cluster.eks_cluster.name
-  fargate_profile_name   = "monitoring_fargate_profile"
+  fargate_profile_name   = "monitoring-fargate-profile"
   pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
   subnet_ids             = aws_subnet.private_subnet[*].id
 
   selector {
     namespace = "monitoring"
     labels = {
-      app = "prometheus"
+      app = "kube-prometheus-stack-prometheus-operator"
     }
   }
 }
