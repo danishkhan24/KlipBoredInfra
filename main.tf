c3281f3de275dc70503b0cc77e8f0396689e473a
Danish Khan <danishkhand24@gmail.com>
2024-09-01 10:28:49 +0100

    Added auth config for k8s

--- main.tf
+++ main.tf
@@ -304,19 +304,24 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.name]
+    command     = "aws"
+  }
 }
 
 data "aws_eks_cluster_auth" "eks_cluster_auth" {
   name = aws_eks_cluster.eks_cluster.name
 }
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = aws_eks_cluster.eks_cluster.endpoint
-#     cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-#     token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
+  }
+}
 
 
 # resource "helm_release" "prometheus" {
