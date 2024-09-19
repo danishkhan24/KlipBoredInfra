0c5673b637fd20a773d8ca28075be999a2024291
Danish Khan <danishkhand24@gmail.com>
2024-09-19 23:34:20 +0100

    Updated directory structure to isolate cluster and k8s resources

---
+++ Kubernetes_Resources/kubernetes_provider.tf
@@ -0,0 +1,21 @@
+data "terraform_remote_state" "eks" {
+  backend = "remote"
+  config = {
+    organization = "Klipbored"
+    workspaces = {
+      name = "EKS_Infrastructure"  # The EKS workspace name
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.eks.outputs.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.eks_cluster_certificate_authority)
+  token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks.outputs.eks_cluster_name]
+    command     = "aws"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,11 @@ output "vpc_id" {
   description = "The ID of the VPC created for EKS."
   value       = aws_vpc.eks_vpc.id
 }
+
+output "eks_cluster_certificate_authority" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
