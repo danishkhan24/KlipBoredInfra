7a024707ce8cba45ecc5ab16747a1b8cc78860d9
Danish Khan <danishkhand24@gmail.com>
2024-09-20 01:59:37 +0100

    Updated ingress

--- EKS_Resources/provider.tf
+++ EKS_Resources/provider.tf
@@ -11,22 +11,16 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"  # Adjust this to your kubeconfig path
-  }
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.name]
-    command     = "aws"
-  }
-}
+# provider "kubernetes" {
+#   host                   = aws_eks_cluster.eks_cluster.endpoint
+#   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.name]
+#     command     = "aws"
+#   }
+# }
 
 data "aws_availability_zones" "available" {
   state = "available"
--- Kubernetes_Resources/ingress.tf
+++ Kubernetes_Resources/ingress.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_ingress" "klipbored-ingress" {
+resource "kubernetes_ingress_v1" "klipbored_ingress" {
   metadata {
     name      = "klipbored-ingress"
     namespace = "default"
@@ -6,44 +6,68 @@ resource "kubernetes_ingress" "klipbored-ingress" {
       "kubernetes.io/ingress.class"                        = "alb"
       "alb.ingress.kubernetes.io/scheme"                   = "internet-facing"
       "alb.ingress.kubernetes.io/target-type"              = "ip"
-      # "alb.ingress.kubernetes.io/listen-ports"             = "[{\"HTTP\": 80}, {\"HTTPS\": 443}]"
-      # "alb.ingress.kubernetes.io/healthcheck-path"         = "/healthz"
-      "alb.ingress.kubernetes.io/certificate-arn"          = "arn:aws:acm:eu-west-2:883463338978:certificate/6a98c78c-ffb7-41b0-a091-3df6a69bc476"  # Optional if using HTTPS
+      "alb.ingress.kubernetes.io/certificate-arn"          = "arn:aws:acm:eu-west-2:883463338978:certificate/6a98c78c-ffb7-41b0-a091-3df6a69bc476"
     }
   }
 
   spec {
+    default_backend {
+      service {
+        name = "frontend-service"
+        port {
+          number = 80
+        }
+      }
+    }
+
     rule {
       host = "klipbored.com"
 
       http {
         path {
           path     = "/"
+          path_type = "Prefix"
 
           backend {
-            service_name = "frontend-service"
-            service_port = 80
+            service {
+              name = "frontend-service"
+              port {
+                number = 80
+              }
+            }
           }
         }
+
         path {
           path     = "/api/"
+          path_type = "Prefix"
 
           backend {
-            service_name = "backend-service"
-            service_port = 80
+            service {
+              name = "backend-service"
+              port {
+                number = 80
+              }
+            }
           }
         }
+
         path {
           path     = "/prometheus"
+          path_type = "Prefix"
 
           backend {
-            service_name = "prometheus"
-            service_port = 9090
+            service {
+              name = "prometheus"
+              port {
+                number = 9090
+              }
+            }
           }
         }
       }
     }
   }
 
-  depends_on = [ kubernetes_service_account.aws_load_balancer_controller_sa ]
+  depends_on = [kubernetes_service_account.aws_load_balancer_controller_sa]
 }
